ccdd165a19e39a73e61d9fc540e7f181c4802dc3
Justin Collier <twoquestions@protonmail.com>
2019-05-20 19:03:22 -0400

    initial commit

---
+++ potter-kata/potter-kata-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace potter_kata_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
