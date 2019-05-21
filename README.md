57b08a03dc7a02b84632422936255f91d11997af
Justin Collier <twoquestions@protonmail.com>
2019-05-20 21:29:44 -0400

    Added a small command line application, and added a bit of documentation.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# potter
-Minimizes book cost
+# Book Price Calculator
+
+Minimizes book cost with the publisher's deal.
+
+### Running the application
+To use the command line application, open the solution in Visual Studio and start up the `potter-kata-tui` project.
+
+### Running the tests
+To run the tests in Visual Studio, go to `Test -> Run -> All Tests`
+
--- potter-kata/potter-kata-tui/Program.cs
+++ potter-kata/potter-kata-tui/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Potter_Kata;
+using System;
 
 namespace potter_kata_tui
 {
@@ -7,10 +8,47 @@ namespace potter_kata_tui
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Harry Potter Book Pricing Application!");
-            Console.WriteLine();
-            var s = Console.ReadLine();
-            Console.WriteLine($"You put in {s}");
-            Environment.Exit(0);
+            string s = "";
+            var priceCalc = new PriceCalculator();
+            while (s != "q")
+            {
+                Console.WriteLine("To add a book, enter a number from 1 to 5");
+                Console.WriteLine("To clear your cart, press c");
+                Console.WriteLine("To quit, press q.");
+                s = Console.ReadLine();
+                switch(s.ToLower())
+                {
+                    case "1":
+                        priceCalc.addBook(new Book(1));
+                        break;
+                    case "2":
+                        priceCalc.addBook(new Book(2));
+                        break;
+                    case "3":
+                        priceCalc.addBook(new Book(3));
+                        break;
+                    case "4":
+                        priceCalc.addBook(new Book(4));
+                        break;
+                    case "5":
+                        priceCalc.addBook(new Book(5));
+                        break;
+                    case "c":
+                        priceCalc.Clear();
+                        break;
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, I didn't understand that.");
+                        break;
+                }
+
+                Console.WriteLine("");
+                var orderPrice = priceCalc.OrderPrice().ToString("N2");
+                Console.WriteLine($@"Your total thus far is: {orderPrice} EUR");
+                Console.WriteLine("");
+            }
         }
     }
 }
